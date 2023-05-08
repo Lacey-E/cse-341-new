a616a00d22ad1a06927753e69c207f7800cb678e
Lacey-E <buchiezim@gmail.com>
2023-05-08 14:44:56 -0600

    eee

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -56,7 +56,7 @@ const getSingle = async (req, res) => {
   
   const deleteContact = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().db('Test').collection('contact').remove({ _id: userId }, true);
+    const response = await mongodb.getDb().db('Test').collection('contact').deleteOne({ _id: userId }, true);
     console.log(response);
     if (response.deletedCount > 0) {
       res.status(204).send();
